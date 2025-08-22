cf21b6f9a53f0a060aaef2407bfe8c76c1b7d116
rsargam <grow.sargam@gmail.com>
2025-08-22 23:52:01 +0530

    Create 2021-RemoveAllOccurrencesOfASubstring

---
+++ 2021-RemoveAllOccurrencesOfASubstring/2021-RemoveAllOccurrencesOfASubstring.cpp
@@ -0,0 +1,10 @@
+// Last updated: 22/08/2025, 23:52:00
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        while(s.length()!=0 && s.find(part)<s.length()){
+            s.erase(s.find(part),part.length());
+        }
+        return s;
+    }
+};
