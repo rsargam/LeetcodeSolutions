a285146e2428bf10bee8494d71df67147867b120
rsargam <grow.sargam@gmail.com>
2025-08-22 23:54:08 +0530

    Create 14-LongestCommonPrefix

---
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.cpp
@@ -0,0 +1,32 @@
+// Last updated: 22/08/2025, 23:54:07
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+       
+        if (strs.empty()) {
+            return "";
+        }
+
+        string ans= "";
+        for(int i=0;i<strs[0].size();i++){
+            char ch= strs[0][i];
+
+            bool match=true;
+            for(int j = 1;j< strs.size();j++){
+                if(strs[j].size() < i || ch!=strs[j][i]){
+                    match=false;
+                    break;
+                }
+               
+            }
+             if(match==false){
+                    break;
+                }
+                else{
+                    ans.push_back(ch);
+                }
+        }
+        return ans;
+
+    }
+};
