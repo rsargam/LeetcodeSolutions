00d876e3e07c6f5736f47957632d4b9ba653ec00
rsargam <grow.sargam@gmail.com>
2025-08-22 23:52:43 +0530

    Create 409-LongestPalindrome

---
+++ 409-LongestPalindrome/409-LongestPalindrome.cpp
@@ -0,0 +1,32 @@
+// Last updated: 22/08/2025, 23:52:42
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        vector<int> lower(26,0),upper(26,0);
+        for(int i=0;i<s.length();i++){
+            if(s[i]>='a')
+            lower[s[i]-'a']++;           
+            else
+            upper[s[i]-'A']++;
+        }
+        int count=0;
+        bool odd=0;
+        for(int i=0;i<26;i++){
+            if(lower[i]%2==0)
+            count+=lower[i];
+            else{
+            count+=lower[i]-1;
+            odd=1;
+            }
+
+            if(upper[i]%2==0)
+            count+=upper[i];
+            else{
+                count+=upper[i]-1;
+                odd=1;
+            }
+            
+        }
+        return count+odd;
+    }
+};
