c133e5b6a037d9408250848d1a4b8962ea336fda
rsargam <grow.sargam@gmail.com>
2025-08-22 23:52:32 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.cpp
@@ -0,0 +1,23 @@
+// Last updated: 22/08/2025, 23:52:32
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int start=0, end=nums.size()-1,mid;
+        while(start<=end){
+            mid=start+(end-start)/2;
+
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                start=mid+1;
+            }
+            else if(nums[mid]>target){
+                end=mid-1;          
+        }
+            
+    
+     }
+     return -1;
+    }
+};
