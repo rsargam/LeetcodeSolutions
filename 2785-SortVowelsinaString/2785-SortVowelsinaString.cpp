3378ff20cddb8d6c5921368323ccf71fa966d3c9
rsargam <grow.sargam@gmail.com>
2025-09-13 01:19:04 +0530

    File updated at 13/09/2025, 01:19:03

--- 2785-SortVowelsinaString/2785-SortVowelsinaString.cpp
+++ 2785-SortVowelsinaString/2785-SortVowelsinaString.cpp
@@ -1,44 +1,12 @@
-// Last updated: 12/09/2025, 00:52:10
+// Last updated: 13/09/2025, 01:19:03
 class Solution {
 public:
-    string sortVowels(string s) {
-        vector<int>lower(26,0), upper(26,0);
-        for(int i=0;i<s.size();i++){
-            if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u'){
-                lower[s[i]-'a']++;
-                s[i]='#';
-            }
-            else if(s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U'){
-                upper[s[i]-'A']++;
-                s[i]='#';
-
-            }
+    bool doesAliceWin(string s) {
+        unordered_set<char>vowels={'a','e','i','o','u'};
+        for(char c: s){
+            if(vowels.count(c))
+            return true;
         }
-
-        string vowel;
-        for(int i=0;i<26;i++){
-            char c= 'A'+i;
-            while(upper[i]){
-                vowel+=c;
-                upper[i]--;
-            }
-        }
-
-        for(int i=0;i<26;i++){
-            char c= 'a'+i;
-            while(lower[i]){
-                vowel+=c;
-                lower[i]--;
-            }
-        }
-        int first=0,second=0;
-        while(second<vowel.size()){
-            if(s[first]=='#'){
-                s[first]=vowel[second];
-                second++;
-            }
-            first++;
-        }
-        return s;
+        return false;
     }
 };
