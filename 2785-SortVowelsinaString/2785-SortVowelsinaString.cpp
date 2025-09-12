29a01209981b87f4e216390db40cfde308b64686
rsargam <grow.sargam@gmail.com>
2025-09-13 01:21:48 +0530

    File updated at 13/09/2025, 01:21:48

--- 2785-SortVowelsinaString/2785-SortVowelsinaString.cpp
+++ 2785-SortVowelsinaString/2785-SortVowelsinaString.cpp
@@ -1,11 +1,10 @@
-// Last updated: 13/09/2025, 01:21:22
+// Last updated: 13/09/2025, 01:21:48
 class Solution {
 public:
     bool doesAliceWin(string s) {
         int count=0;
         for(char c: s){
-            if(c=='a' ||c=='e' ||c=='i' ||c=='o' ||c=='u')
-            count++;
+            if(c=='a' ||c=='e' ||c=='i' ||c=='o' ||c=='u')count++;
         }
         if(count==0)
         return false;
