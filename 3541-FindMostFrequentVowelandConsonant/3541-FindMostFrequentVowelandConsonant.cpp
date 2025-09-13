0fce93b5aa76bfc074e6ede839cc83ff574f4a57
rsargam <grow.sargam@gmail.com>
2025-09-13 22:26:09 +0530

    Create 3541-FindMostFrequentVowelandConsonant

---
+++ 3541-FindMostFrequentVowelandConsonant/3541-FindMostFrequentVowelandConsonant.cpp
@@ -0,0 +1,20 @@
+// Last updated: 13/09/2025, 22:26:09
+class Solution {
+public:
+    int maxFreqSum(string s) {
+        vector<int>freq(26);
+        int maxcons=0,maxvowel=0;
+        for(char c: s){
+            int i=c-'a';
+            freq[i]++;
+            if(c=='a'|| c=='e'||c=='i'||c=='o'||c=='u'){
+                maxvowel=max(maxvowel,freq[i]);
+            }
+            else{
+                maxcons=max(maxcons,freq[i]);
+            }
+             
+        }
+        return maxcons+maxvowel;
+    }
+};
