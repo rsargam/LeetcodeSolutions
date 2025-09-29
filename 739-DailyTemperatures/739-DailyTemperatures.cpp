207957e03f988b0d06197e4dae96e16422686715
rsargam <grow.sargam@gmail.com>
2025-09-29 16:28:21 +0530

    File updated at 29/09/2025, 16:28:21

--- 739-DailyTemperatures/739-DailyTemperatures.cpp
+++ 739-DailyTemperatures/739-DailyTemperatures.cpp
@@ -1,18 +1,29 @@
-// Last updated: 29/09/2025, 16:27:42
+// Last updated: 29/09/2025, 16:28:21
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        stack<int>st;
-        int n=temperatures.size();
-        vector<int>answer(n,0);
-        for(int i=0;i<n;i++){
-           while(!st.empty() && temperatures[i]>temperatures[st.top()]){
-            int index=st.top();
-            st.pop();
-            answer[index]=i-index;
-           }
-           st.push(i);
+        
+        vector<int> result(temperatures.size());
+
+        // [89,62,70,58,47,47,46,76,100,70]
+        //                           *   0
+
+        // 70
+
+        vector<int> stack;
+
+        for (int i = temperatures.size() - 1; i >= 0; --i)
+        {
+            const auto t = temperatures[i];
+
+            while (!stack.empty() && temperatures[stack.back()] <= t)
+                stack.pop_back();
+
+            result[i] = stack.empty() ? 0 : stack.back() - i;
+
+            stack.push_back(i);
         }
-        return answer;
+
+        return result;
     }
 };
