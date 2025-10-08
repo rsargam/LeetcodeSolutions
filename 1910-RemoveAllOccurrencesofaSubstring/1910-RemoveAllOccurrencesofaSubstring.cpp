c03b27499bfcce1f1cb175a95d200b9c761fffe8
rsargam <grow.sargam@gmail.com>
2025-10-08 22:52:55 +0530

    Create 1910-RemoveAllOccurrencesofaSubstring

---
+++ 1910-RemoveAllOccurrencesofaSubstring/1910-RemoveAllOccurrencesofaSubstring.cpp
@@ -0,0 +1,10 @@
+// Last updated: 08/10/2025, 22:52:54
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        while(s.length()!=0 && s.find(part)<s.length()){
+            s.erase(s.find(part),part.length());
+        }
+        return s;
+    }
+};
