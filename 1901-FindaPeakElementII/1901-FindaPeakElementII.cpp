2238dd99d7a175f1a9f6b38970da23a29c3300ec
rsargam <grow.sargam@gmail.com>
2025-10-11 02:46:43 +0530

    Create 1901-FindaPeakElementII

---
+++ 1901-FindaPeakElementII/1901-FindaPeakElementII.cpp
@@ -0,0 +1,25 @@
+// Last updated: 11/10/2025, 02:46:42
+class Solution {
+public:
+    vector<int> findPeakGrid(vector<vector<int>>& mat) {
+        int first=0,last=mat.size()-1;
+        int peak=-1;
+
+        while(first<last){
+            auto m=(first+last)/2;
+            auto curr=*max_element(mat[m].begin(),mat[m].end());
+            auto next=*max_element(mat[m+1].begin(),mat[m+1].end());
+            if(curr>next){
+                last=m;
+                peak=curr;
+            }
+            else{
+                first=m+1;
+                peak=next;
+            }
+        }
+        int col=max_element(mat[first].begin(),mat[first].end())-mat[first].begin();
+
+        return{first,col};
+    }
+};
