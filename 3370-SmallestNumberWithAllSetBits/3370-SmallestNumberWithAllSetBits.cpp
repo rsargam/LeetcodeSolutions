4b3764bb7e6b0eb652c119669fdd4ece308b05a1
rsargam <grow.sargam@gmail.com>
2025-10-30 01:18:25 +0530

    Create 3370-SmallestNumberWithAllSetBits

---
+++ 3370-SmallestNumberWithAllSetBits/3370-SmallestNumberWithAllSetBits.cpp
@@ -0,0 +1,11 @@
+// Last updated: 30/10/2025, 01:18:24
+class Solution {
+public:
+    int smallestNumber(int n) {
+        int x=1;
+        while(x-1<n){
+            x<<=1;
+        }
+        return x-1;
+    }
+};
