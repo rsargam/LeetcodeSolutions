dc0704da9e5cf727f00438ac057de0921285f9dd
rsargam <grow.sargam@gmail.com>
2025-11-17 23:52:54 +0530

    Create 860-LemonadeChange

---
+++ 860-LemonadeChange/860-LemonadeChange.cpp
@@ -0,0 +1,32 @@
+// Last updated: 17/11/2025, 23:52:54
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int five=0,ten=0;
+        for(int i=0;i<bills.size();i++){
+            if(bills[i]==5)
+            five++;
+            else if(bills[i]==10){
+                if(five==0)
+                return 0;
+                    five--;
+                    ten++;
+            } 
+                
+            else{
+                if(ten>0 && five>0){
+                    ten--;
+                    five--;
+                }
+                else if(five>=3){
+                    five-=3;
+                }
+                else{
+                     return false;
+                }    
+            }
+
+            }
+             return true;
+        }
+};
