91365dd83c24f3ea0c486c82382572004329e4df
rsargam <grow.sargam@gmail.com>
2025-11-19 20:12:19 +0530

    Create 2154-KeepMultiplyingFoundValuesbyTwo

---
+++ 2154-KeepMultiplyingFoundValuesbyTwo/2154-KeepMultiplyingFoundValuesbyTwo.cpp
@@ -0,0 +1,18 @@
+// Last updated: 19/11/2025, 20:12:19
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int original) {
+        bitset<1001> seen=0;
+        for(int x:nums){
+            seen[x]=1;
+        }
+        int x=original;
+        while(x<=1000){
+            if(seen[x])
+            x*=2;
+            else
+            break;
+        }
+        return x;
+    }
+};
