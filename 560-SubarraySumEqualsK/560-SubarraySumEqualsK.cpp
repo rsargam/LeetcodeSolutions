ddfaa5ac0c615de94c628a78f63bb052affc646d
rsargam <grow.sargam@gmail.com>
2026-01-21 12:16:55 +0530

    File updated at 21/01/2026, 12:16:54

--- 560-SubarraySumEqualsK/560-SubarraySumEqualsK.cpp
+++ 560-SubarraySumEqualsK/560-SubarraySumEqualsK.cpp
@@ -1,20 +1,19 @@
-// Last updated: 22/08/2025, 23:52:37
-class Solution {
-public:
-    int subarraySum(vector<int>& nums, int k) {
-        int ans=0,sum=0;
-        for(int i=0;i<nums.size();i++){
-            sum=nums[i];
-            if(sum==k)
-            ans++;
-
-            for(int j=i+1;j<nums.size();j++){
-                sum+=nums[j];
-                if(sum==k)
-                 ans++;
-            }
-            
-        }
-        return ans;
-    }
-};
+// Last updated: 21/01/2026, 12:16:54
+1class Solution {
+2public:
+3    int subarraySum(vector<int>& nums, int k) {
+4        int n=nums.size();
+5        vector<int>prefix(n+1,0);
+6        for(int i=0;i<n;i++){
+7            prefix[i+1]=prefix[i]+nums[i];
+8        }
+9        int count=0;
+10        for(int i=0;i<n;i++){
+11            for(int j=i;j<n;j++){
+12                if(prefix[j+1]-prefix[i]==k)
+13                count++;
+14            }
+15        }
+16        return count;
+17    }
+18};
