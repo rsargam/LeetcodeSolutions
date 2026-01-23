529664e159e0c6e8c013d7861d6d05500ecb2105
rsargam <grow.sargam@gmail.com>
2026-01-23 10:33:24 +0530

    File updated at 23/01/2026, 10:33:24

--- 150-EvaluateReversePolishNotation/150-EvaluateReversePolishNotation.cpp
+++ 150-EvaluateReversePolishNotation/150-EvaluateReversePolishNotation.cpp
@@ -1,30 +1,35 @@
-// Last updated: 03/11/2025, 17:57:43
-class Solution {
-public:
-    int evalRPN(vector<string>& tokens) {
-        unordered_map<string,function<int(int,int)>>mp={
-            {"+",[](int a,int b){return a+b;}},
-            {"-",[](int a,int b){return a-b;}},
-            {"*",[](int a,int b){return a*b;}},
-            {"/",[](int a,int b){return a/b;}}
-        };
-        stack<int>st;
-        for( string &token :tokens){
-            if(token=="+" || token=="-" ||token=="/" ||token=="*"){
-
-            
-            int b=st.top();
-            st.pop();
-            int a=st.top();
-            st.pop();
-
-            int result=mp[token](a,b);
-            st.push(result);
-            }
-            else{
-                st.push(stoi(token));
-            }
-        }
-        return st.top();
-    }
-};
+// Last updated: 23/01/2026, 10:33:24
+1class Solution {
+2public:
+3    bool isNumber(string s){
+4        return !(s=="+" || s=="-" || s=="*" || s=="/");
+5    }
+6    int evalRPN(vector<string>& tokens) {
+7        stack<int>st;
+8        for(int i=0;i<tokens.size();i++){
+9            if(isNumber(tokens[i])){
+10                st.push(stoi(tokens[i]));
+11            }
+12            else {
+13                int a=st.top();
+14                st.pop();
+15                int b=st.top();
+16                st.pop();
+17               if(tokens[i]=="+"){
+18                st.push(a+b);
+19               }
+20               else if(tokens[i]=="-"){
+21                st.push(b-a);
+22               }
+23               else if(tokens[i]=="*"){
+24                st.push(a*b);
+25               }
+26               else if(tokens[i]=="/"){
+27                st.push(b/a);
+28               }
+29
+30            }
+31        }
+32        return st.top();
+33    }
+34};
