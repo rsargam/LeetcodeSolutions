9b7ee3aa34e907a2007cd1f987d062ab4cbaf9a6
rsargam <grow.sargam@gmail.com>
2026-04-19 18:52:16 +0530

    File updated at 19/04/2026, 18:52:16

--- 70-ClimbingStairs/70-ClimbingStairs.cpp
+++ 70-ClimbingStairs/70-ClimbingStairs.cpp
@@ -1,17 +1,17 @@
-// Last updated: 22/08/2025, 23:53:40
-class Solution {
-public:
-    int climbStairs(int n) {
-        if(n<=1){
-            return 1;
-        }
-       int prev1=1;
-       int prev2=1;
-       for(int i=2;i<=n; i++){
-        int current= prev1 + prev2;
-        prev2=prev1;
-        prev1=current;
-       }
-       return prev1;
-    }
-};
+// Last updated: 19/04/2026, 18:52:16
+1class Solution {
+2public:
+3    int climbStairs(int n) {
+4        if(n<=1){
+5            return 1;
+6        }
+7       int prev1=1;
+8       int prev2=1;
+9       for(int i=2;i<=n; i++){
+10        int current= prev1 + prev2;
+11        prev2=prev1;
+12        prev1=current;
+13       }
+14       return prev1;
+15    }
+16};
