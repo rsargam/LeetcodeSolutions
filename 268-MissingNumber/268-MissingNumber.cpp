d0b1bd1d9e0130c4a63f4cb4a4b8d2e12a231e43
rsargam <grow.sargam@gmail.com>
2026-06-02 12:14:20 +0530

    File updated at 02/06/2026, 12:14:19

--- 268-MissingNumber/268-MissingNumber.cpp
+++ 268-MissingNumber/268-MissingNumber.cpp
@@ -1,17 +1,13 @@
-// Last updated: 22/08/2025, 23:52:53
-class Solution {
-public:
-    int missingNumber(vector<int>& nums) {
-        int n = nums.size();
-        int sum = n * (n + 1) / 2;
-        int sum_orig = 0;
-        
-     
-        for (int num : nums) {
-            sum_orig += num;
-        }
-        
-    
-        return sum - sum_orig;
-    }
-};
+// Last updated: 02/06/2026, 12:14:19
+1class Solution {
+2public:
+3    int missingNumber(vector<int>& nums) {
+4        int n=nums.size();
+5        int sum=(n*(n+1))/2;
+6        int originalsum=0;
+7        for(int num :nums){
+8            originalsum +=num;
+9        }
+10        return sum-originalsum;
+11    }
+12};
