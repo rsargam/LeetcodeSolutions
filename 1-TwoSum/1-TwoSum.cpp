e633f11a80af6fdd6326e2f33188d3fff0142094
rsargam <grow.sargam@gmail.com>
2026-06-02 12:46:45 +0530

    File updated at 02/06/2026, 12:46:45

--- 1-TwoSum/1-TwoSum.cpp
+++ 1-TwoSum/1-TwoSum.cpp
@@ -1,15 +1,18 @@
-// Last updated: 21/01/2026, 09:49:48
+// Last updated: 02/06/2026, 12:46:45
 1class Solution {
 2public:
 3    vector<int> twoSum(vector<int>& nums, int target) {
 4        int n=nums.size();
-5        for(int i=0;i<n;i++){
-6            for(int j=i+1;j<n;j++){
-7                if(nums[j]+nums[i]==target){
-8                    return {i,j};
-9                }
-10            }
-11        }
-12        return { };
-13    }
-14};
+5        vector<int>ans;
+6        for(int i=0;i<n;i++){
+7            for (int j=i+1;j<n;j++){
+8                if(nums[i]+nums[j]==target)
+9               return {i,j};
+10                
+11               
+12
+13            }
+14        }
+15        return {};
+16    }
+17};
