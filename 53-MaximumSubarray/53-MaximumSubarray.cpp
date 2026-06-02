797e1e04627ca572def0f6a3674677f476c5a880
rsargam <grow.sargam@gmail.com>
2026-06-02 14:47:26 +0530

    File updated at 02/06/2026, 14:47:25

--- 53-MaximumSubarray/53-MaximumSubarray.cpp
+++ 53-MaximumSubarray/53-MaximumSubarray.cpp
@@ -1,17 +1,17 @@
-// Last updated: 02/06/2026, 14:37:01
+// Last updated: 02/06/2026, 14:47:25
 1class Solution {
 2public:
 3    int maxSubArray(vector<int>&nums) {
 4        int n=nums.size();
-5        int sum=0, max=INT_MIN;
+5        int sum=0, maxi=INT_MIN;
 6        for(int i=0;i<n;i++){
 7            sum+=nums[i];
-8            if(sum>max)
-9            max=sum;
+8           maxi=max(sum,maxi);
+9            
 10            if(sum<0)
 11            sum=0;
 12        }
-13        return max;
+13        return maxi;
 14        
 15    }
 16};
