87e2d392c6bc7dcccea9d7df7b37c8469a1f8399
rsargam <grow.sargam@gmail.com>
2026-06-06 10:54:55 +0530

    File updated at 06/06/2026, 10:54:55

--- 21-MergeTwoSortedLists/21-MergeTwoSortedLists.cpp
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.cpp
@@ -1,40 +1,40 @@
-// Last updated: 22/08/2025, 23:54:02
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
-public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-       ListNode* newnode=new ListNode(0);
-       ListNode* current=newnode;
-
-       while(list1!=NULL && list2!=NULL){
-        if(list1->val < list2->val){
-            current->next=list1;
-            list1=list1->next;
-        }
-        else{
-             current->next=list2;
-            list2=list2->next;
-        }
-        current=current->next;
-       }
-        if(list1!=NULL){
-            current->next=list1;
-        }
-
-        if(list2!=NULL){
-            current->next=list2;
-        }
-
-     
-       return newnode->next;
-    }
-};
+// Last updated: 06/06/2026, 10:54:55
+1/**
+2 * Definition for singly-linked list.
+3 * struct ListNode {
+4 *     int val;
+5 *     ListNode *next;
+6 *     ListNode() : val(0), next(nullptr) {}
+7 *     ListNode(int x) : val(x), next(nullptr) {}
+8 *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+9 * };
+10 */
+11class Solution {
+12public:
+13    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+14       ListNode* newnode=new ListNode(0);
+15       ListNode* current=newnode;
+16
+17       while(list1!=NULL && list2!=NULL){
+18        if(list1->val < list2->val){
+19            current->next=list1;
+20            list1=list1->next;
+21        }
+22        else{
+23             current->next=list2;
+24            list2=list2->next;
+25        }
+26        current=current->next;
+27       }
+28        if(list1!=NULL){
+29            current->next=list1;
+30        }
+31
+32        if(list2!=NULL){
+33            current->next=list2;
+34        }
+35
+36     
+37       return newnode->next;
+38    }
+39};
